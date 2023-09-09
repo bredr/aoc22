2432d575e2d5b5ba5542ea5f7a0e0c1c85fe60a2
bredr <davidbrett88@gmail.com>
2023-09-09 21:11:05 +0100

    day8 part 2

--- js/src/day8.ts
+++ js/src/day8.ts
@@ -57,3 +57,44 @@ for (let x = 1; x < input.length - 1; x++) {
 }
 
 console.log("part1 total visible", internalCount + edgeCount)
+
+let maxScenicScore = 0;
+for (let x = 1; x < input.length - 1; x++) {
+    for(let y=1; y< input[0].length -1; y++) {
+        const height = input[x][y]
+        let left = 0;
+        for(let xx = x-1; xx >= 0; xx--) {
+            left++;
+            if(input[xx][y]>=height) {
+                break;
+            }
+        }
+        let right = 0;
+        for(let xx = x+1; xx < input.length; xx++) {
+            right++;
+            if(input[xx][y]>=height) {
+                break;
+            }
+        }
+        let up = 0;
+        for(let yy = y+1; yy < input[0].length; yy++) {
+            up++;
+            if(input[x][yy]>=height) {
+                break;
+            }
+        }
+        let down = 0;
+        for(let yy = y-1; yy >=0; yy--) {
+            down++;
+            if(input[x][yy]>=height) {
+                break;
+            }
+        }
+        const scenicScore = left * right * down * up;
+        if (scenicScore > maxScenicScore) {
+            maxScenicScore = scenicScore;
+        }
+    }
+}
+
+console.log("part2 max scenic score", maxScenicScore)
