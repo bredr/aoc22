a8265fabb14c723a9c336aec461b047897c800b7
bredr <davidbrett88@gmail.com>
2023-09-09 22:04:02 +0100

    day9 part2

--- js/src/day9.ts
+++ js/src/day9.ts
@@ -32,19 +32,72 @@ lines.forEach((line) => {
       }
       const dx = Hx - Tx;
       const dy = Hy - Ty;
-      if(dy == 0) {
+      if (dy == 0) {
         Tx += dx > 0 ? 1 : -1;
-      }
-      else if (dx == 0) {
+      } else if (dx == 0) {
+        Ty += dy > 0 ? 1 : -1;
+      } else {
+        Tx += dx > 0 ? 1 : -1;
         Ty += dy > 0 ? 1 : -1;
       }
-      else {
-        Tx += dx > 0 ? 1 : -1
-        Ty += dy > 0 ? 1 : -1 
-      }
-      history.push(`${Tx},${Ty}`)
+      history.push(`${Tx},${Ty}`);
     });
 });
 
-const uniquePlaces = new Set(history)
-console.log("places visited",uniquePlaces.size)
+const uniquePlaces = new Set(history);
+console.log("places visited", uniquePlaces.size);
+
+const positions = Array(10)
+  .fill(0)
+  .map(() => [0, 0]);
+const historyPart2: string[] = ["0,0"];
+lines.forEach((line) => {
+  const [dir, d] = line.split(" ");
+  const distance = parseInt(d);
+  Array(distance)
+    .fill(0)
+    .forEach(() => {
+      switch (dir) {
+        case "R":
+          positions[0][0] += 1;
+          break;
+        case "L":
+          positions[0][0] -= 1;
+          break;
+        case "U":
+          positions[0][1] += 1;
+          break;
+        case "D":
+          positions[0][1] -= 1;
+          break;
+        default:
+          break;
+      }
+      for (let i = 1; i <= 9; i++) {
+        if (
+          positions[i - 1][0] - 1 <= positions[i][0] &&
+          positions[i][0] <= positions[i - 1][0] + 1 &&
+          positions[i - 1][1] - 1 <= positions[i][1] &&
+          positions[i][1] <= positions[i - 1][1] + 1
+        ) {
+          continue;
+        }
+        const dx = positions[i - 1][0] - positions[i][0];
+        const dy = positions[i - 1][1] - positions[i][1];
+        if (dy == 0) {
+          positions[i][0] += dx > 0 ? 1 : -1;
+        } else if (dx == 0) {
+          positions[i][1] += dy > 0 ? 1 : -1;
+        } else {
+          positions[i][0] += dx > 0 ? 1 : -1;
+          positions[i][1] += dy > 0 ? 1 : -1;
+        }
+        if (i == 9) {
+          historyPart2.push(`${positions[i][0]},${positions[i][1]}`);
+        }
+      }
+    });
+});
+
+const uniquePlacesPart2 = new Set(historyPart2);
+console.log("places visited part2", uniquePlacesPart2.size);
