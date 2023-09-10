d1e3d3c776bbdbff099cf9df9276a7f2805e7c08
bredr <davidbrett88@gmail.com>
2023-09-10 09:48:29 +0100

    day10 part2

--- go/day10/main.go
+++ go/day10/main.go
@@ -20,12 +20,18 @@ func main() {
 
 	fileScanner.Split(bufio.ScanLines)
 
+	var lines []string
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		lines = append(lines, line)
+	}
+	readFile.Close()
+
 	x := 1
 	cycle := 0
 	sum := 0
 	cycles := []int{20, 60, 100, 140, 180, 220}
-	for fileScanner.Scan() {
-		line := fileScanner.Text()
+	for _, line := range lines {
 		if strings.Contains(line, "noop") {
 			cycle++
 			if slices.Contains(cycles, cycle) {
@@ -46,5 +52,50 @@ func main() {
 		}
 	}
 	fmt.Printf("part 1: %d\n", sum)
-	readFile.Close()
+
+	x = 1
+	cycle = 0
+	for _, line := range lines {
+		if strings.Contains(line, "noop") {
+			if x-1 <= cycle && cycle <= x+1 {
+				fmt.Print("#")
+			} else {
+				fmt.Print(".")
+			}
+
+			cycle++
+			if cycle%40 == 0 {
+				fmt.Print("\n")
+				cycle = 0
+			}
+		} else {
+			addx := strings.Split(line, " ")
+			v, _ := strconv.Atoi(addx[1])
+			if x-1 <= cycle && cycle <= x+1 {
+				fmt.Print("#")
+			} else {
+				fmt.Print(".")
+			}
+
+			cycle++
+			if cycle%40 == 0 {
+				fmt.Print("\n")
+				cycle = 0
+			}
+			if x-1 <= cycle && cycle <= x+1 {
+				fmt.Print("#")
+			} else {
+				fmt.Print(".")
+			}
+
+			cycle++
+			if cycle%40 == 0 {
+				fmt.Print("\n")
+				cycle = 0
+			}
+			x += v
+		}
+	}
+	fmt.Print("\n")
+
 }
