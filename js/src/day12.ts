e1ad478cc874aa3dff191adb91bf691ac1540602
bredr <davidbrett88@gmail.com>
2023-09-16 18:13:49 +0100

    day12 part2

--- js/src/day12.ts
+++ js/src/day12.ts
@@ -8,52 +8,57 @@ const dirs = [
   [-1, 0],
 ];
 
-const starts: [number, number][] = [];
-let end;
-const map = input.split("\n").map((line, i) =>
-  line.split("").map((char, j) => {
-    let elevation;
-    if (char === "S") {
-      elevation = 0;
-      starts.push([i, j]);
-    } else if (char === "E") {
-      elevation = 25;
-      end = [i, j];
-    } else {
-      elevation = char.codePointAt(0)! - "a".codePointAt(0)!;
-    }
-    return elevation;
-  })
-);
-if (!end) {
-  console.error("end undefined");
-  process.exit(1);
-}
+function solve(input: string, part: 1 | 2) {
+  const starts: [number, number][] = [];
+  let end;
+  const map = input.split("\n").map((line, i) =>
+    line.split("").map((char, j) => {
+      let elevation;
+      if (char === "S" || (part === 2 && char === "a")) {
+        elevation = 0;
+        starts.push([i, j]);
+      } else if (char === "E") {
+        elevation = 25;
+        end = [i, j];
+      } else {
+        elevation = char.codePointAt(0)! - "a".codePointAt(0)!;
+      }
+      return elevation;
+    })
+  );
+  if (!end) {
+    console.error("end undefined");
+    return;
+  }
 
-const queue = starts.map((start) => ({ pos: start, steps: 0 }));
-const seen: [number, number][] = [];
-while (queue.length) {
-  const {
-    pos: [i, j],
-    steps,
-  } = queue.shift()!;
-  if (seen[i]?.[j]) {
-    continue;
-  }
-  if (i === end[0] && j === end[1]) {
-    console.log(steps);
-    break;
-  }
-  for (const [di, dj] of dirs) {
-    if (
-      map[i + di]?.[j + dj] === undefined ||
-      map[i + di][j + dj] > map[i][j] + 1 ||
-      seen[i + di]?.[j + dj]
-    ) {
+  const queue = starts.map((start) => ({ pos: start, steps: 0 }));
+  const seen: [number, number][] = [];
+  while (queue.length) {
+    const {
+      pos: [i, j],
+      steps,
+    } = queue.shift()!;
+    if (seen[i]?.[j]) {
       continue;
     }
-    queue.push({ pos: [i + di, j + dj], steps: steps + 1 });
+    if (i === end[0] && j === end[1]) {
+      console.log("part", part, "shortest path", steps);
+      break;
+    }
+    for (const [di, dj] of dirs) {
+      if (
+        map[i + di]?.[j + dj] === undefined ||
+        map[i + di][j + dj] > map[i][j] + 1 ||
+        seen[i + di]?.[j + dj]
+      ) {
+        continue;
+      }
+      queue.push({ pos: [i + di, j + dj], steps: steps + 1 });
+    }
+    seen[i] = seen[i] ?? [];
+    seen[i][j] = 1;
   }
-  seen[i] = seen[i] ?? [];
-  seen[i][j] = 1;
 }
+
+solve(input, 1);
+solve(input, 2);
